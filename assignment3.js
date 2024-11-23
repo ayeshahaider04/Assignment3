cf876b7533af5de2a2fe2b68f8c24402ba4aad83
ayeshahaider04 <ayesha.haider@ontaiotechu.net>
2024-11-23 17:09:34 -0500

    Working on functionality

--- assignment3.js
+++ assignment3.js
@@ -6,14 +6,23 @@ const itemRoutes = require('./server/routes/items');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// Set views directory and view engine
+app.set('views', path.join(__dirname, 'server', 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect to MongoDB
 connectDB();
 
+// Routes
 app.use('/', itemRoutes);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
