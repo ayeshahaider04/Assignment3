f37a396a95d88e28f7e1e9b9ae35c37baef2624f
ayeshahaider04 <ayesha.haider@ontaiotechu.net>
2024-11-23 17:16:05 -0500

    Changing files

--- assignment3.js
+++ assignment3.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const path = require('path');
-const connectDB = require('./server/config/db');
-const itemRoutes = require('./server/routes/items');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set views directory and view engine
 app.set('views', path.join(__dirname, 'server', 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware
+// Middleware to parse URL-encoded form data
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Connect to MongoDB
-connectDB();
-
-// Routes
+// Import and use routes
+const itemRoutes = require('./server/routes/items');
 app.use('/', itemRoutes);
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
 
 
